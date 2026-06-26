dcf5f004e480d5e72228440d807700ebce553154
nisatsama <nisatsama7547@gmail.com>
2026-06-26 13:34:22 +0530

    Planner Page added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,15 +3,17 @@ import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 import TaskForm from "./components/TaskForm/TaskForm";
 import Today from "./pages/TodayPage/Today";
+import Planner from "./pages/PlannerPage/Planner";
 
 const App = () => {
   return (
     <div>
-      <Navbar />
+      {/* <Navbar /> */}
 
       <Routes>
         <Route path="/" element={<TaskForm />} />
         <Route path="/today" element={<Today />} />
+        <Route path="/planner" element={<Planner />} />
       </Routes>
     </div>
   );
